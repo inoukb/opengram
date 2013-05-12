7216718376488968755e4e0560f2f47d7616eebf
Rav3nL0g <etiennebondot@gmail.com>
2013-05-12 02:07:02 +0200

    mobile : main.js upload_picture

--- mobile/android/assets/www/main.js
+++ mobile/android/assets/www/main.js
@@ -1,10 +1,11 @@
 
-function initialize() {
+function initialize() {
     $('#picture').hide();
     $('#save_picture').hide();
     $('#cancel').hide();
     $('#take_picture').show();
-}   
+}
+  
 function captureImage() {
     navigator.device.capture.captureImage(
             function(files){
@@ -19,22 +20,65 @@ function captureImage() {
             console.log('Error');
             }, {limit: 1});
 }
+
 function affichePosition(pos) {
     $('#debug').append('AFFICHE POS : ' + pos.coords.latitude + ' / ' + pos.coords.longitude + '<br />');
     console.log('AFFICHE POS<br />');
+    var options = new FileUploadOptions();
+    var ft = new FileTransfer();
+    var params = new Object();
+    var server = "http://opengram.appspot.com/receive";
+    var name = $('#picture').attr('src');
+    var win = function(r) {
+	console.log("Code = " + r.responseCode);
+	console.log("Response = " + r.response);
+	console.log("Sent = " + r.bytesSent);
+    }
+    var fail = function(error) {
+	alert("error");
+	switch (error.code) {
+	case FileTransferError.FILE_NOT_FOUND_ERR:
+            alert("Photo file not found");
+            break;
+	case FileTransferError.INVALID_URL_ERR:
+	    alert("Bad Photo URL");
+	    break;
+	case FileTransferError.CONNECTION_ERR:
+	    alert("Connection error");
+	    break;
+	}
+	console.log("upload error source " + error.source);
+	console.log("upload error target " + error.target);
+    }
+    options.fileKey = "file";
+    options.fileName = name.substr(name.lastIndexOf('/') + 1);
+    options.mimeType = "image/jpeg";
+    options.chunkedMode = false;
+    params.location = pos.coords.latitude + "," + pos.coords.longitude;
+    params.datetime = pos.timestamp;
+    params.phone_id = device.uuid;
+    params.comment = '';
+    options.params = params;
+    $('#debug').html('filekey = ' + options.fileKey + '<br />' + 
+	'filename = ' + options.fileName + '<br />' + 
+	'mimeType = ' + options.mimeType + '<br />' +
+	'location = ' + params.location + '<br />' +
+	'datetime = ' + params.datetime + '<br />' +
+	'phone_id = ' + params.phone_id + '<br />' +
+	'comment = ' + params.comment + '<br />');	
+    ft.upload(name, server, win, fail, options);
 }
+
 function errorPosition(error) {
     $('#debug').append('ERROR POS<br />');
     console.log('ERROR POS<br />');
 }
+
 function save_picture() {
     console.log('Image : ' + $('#picture').attr('src'));
     if (navigator.geolocation) {
         $('#debug').append('YESSS<br />');
-        navigator.geolocation.getCurrentPosition(affichePosition,errorPosition);
-        //setTimeout(function() {
-        //    navigator.geolocation.getCurrentPosition(affichePosition,errorPosition);
-        //}, 1500);
+        navigator.geolocation.getCurrentPosition(affichePosition, errorPosition);
     }
     else
         $('#debug').append('NOOOO<br />');
