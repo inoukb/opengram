6f276b3a24c52f825b51556b8bc0e373cea518f7
Platane <arthur@arthur-brongniart.fr>
2013-05-12 04:45:11 +0200

    gram list

--- site plugin/GramMap.js
+++ site plugin/GramMap.js
@@ -43,69 +43,154 @@ GramListener.prototype={
 		var getRandomLng=function(){
 			return 6.18 + ( Math.random() - 0.5 ) * 0.1;
 		};
-
+		
 		window.setTimeout( $.proxy(function(){
-
-			this.elements.push( new GramElement(
+			
+			this.elements.unshift( new GramElement(
 				"http://www.allisonandbusby.com/info/wp-content/uploads/2010/05/a-happy-martin-edwards.jpg",
 				getRandomLat(),
 				getRandomLng(),
 				"iamhappy"
 			));
-			this.elements.push( new GramElement(
-				"",
-				getRandomLat(),
-				getRandomLng(),
-				"sodoi"
-			));
-			this.elements.push( new GramElement(
-				"",
-				getRandomLat(),
-				getRandomLng(),
-				"metoo"
-			));
-			this.elements.push( new GramElement(
-				"",
-				getRandomLat(),
-				getRandomLng(),
-				"alsoi"
-			));
-			this.elements.push( new GramElement(
+
+			this.trigger("add",this.elements[ 0] );
+
+			this.elements.unshift( new GramElement(
 				"",
 				getRandomLat(),
 				getRandomLng(),
 				"sodoi"
 			));
 
+			this.trigger("add",this.elements[ 0] );
+
+			this.elements.unshift( new GramElement(
+				"",
+				getRandomLat(),
+				getRandomLng(),
+				"metoo"
+			));
+
+			this.trigger("add",this.elements[ 0] );
+
+			this.elements.unshift( new GramElement(
+				"",
+				getRandomLat(),
+				getRandomLng(),
+				"alsoi"
+			));
+
+			this.trigger("add",this.elements[ 0] );
+
+			this.elements.unshift( new GramElement(
+				"",
+				getRandomLat(),
+				getRandomLng(),
+				"sodoi"
+			));
+
+			this.trigger("add",this.elements[ 0] );
 			this.trigger("change");
 
 		},this),1000);
 
 		window.setTimeout( $.proxy(function(){
 
-			this.elements.push( new GramElement(
+			this.elements.unshift( new GramElement(
 				"http://www.allisonandbusby.com/info/wp-content/uploads/2010/05/a-happy-martin-edwards.jpg",
 				getRandomLat(),
 				getRandomLng(),
 				"iamhappy"
 			));
-			this.elements.push( new GramElement(
+			this.trigger("add",this.elements[ 0] );
+
+			this.elements.unshift( new GramElement(
 				"",
 				getRandomLat(),
 				getRandomLng(),
 				"sodoi"
 			));
 			
+			this.trigger("add",this.elements[ 0] );
 			this.trigger("change");
 
-		},this),180000);
+		},this),1800);
+
+		window.setTimeout( $.proxy(function(){
+
+			this.elements.unshift( new GramElement(
+				"http://www.allisonandbusby.com/info/wp-content/uploads/2010/05/a-happy-martin-edwards.jpg",
+				getRandomLat(),
+				getRandomLng(),
+				"iamhappy"
+			));
+
+			this.trigger("add",this.elements[ 0] );
+
+			this.elements.unshift( new GramElement(
+				"",
+				getRandomLat(),
+				getRandomLng(),
+				"sodoi"
+			));
+			
+			this.trigger("add",this.elements[ 0] );
+			this.trigger("change");
+
+		},this),3800);
+	
+
+		window.setTimeout( $.proxy(function(){
+
+			this.elements.unshift( new GramElement(
+				"http://www.allisonandbusby.com/info/wp-content/uploads/2010/05/a-happy-martin-edwards.jpg",
+				getRandomLat(),
+				getRandomLng(),
+				"iamhappy"
+			));
+			
+			this.trigger("add",this.elements[ 0] );
+			this.trigger("change");
+
+		},this),100);
+
+		window.setTimeout( $.proxy(function(){
+
+			this.elements.unshift( new GramElement(
+				"http://www.allisonandbusby.com/info/wp-content/uploads/2010/05/a-happy-martin-edwards.jpg",
+				getRandomLat(),
+				getRandomLng(),
+				"iamhappy"
+			));
+			
+			this.trigger("add",this.elements[ 0] );
+			this.trigger("change");
+
+		},this),5000);
+
+		window.setTimeout( $.proxy(function(){
+
+			this.elements.unshift( new GramElement(
+				"http://www.allisonandbusby.com/info/wp-content/uploads/2010/05/a-happy-martin-edwards.jpg",
+				getRandomLat(),
+				getRandomLng(),
+				"iamhappy"
+			));
+			
+			this.trigger("add",this.elements[ 0] );
+			this.trigger("change");
+
+		},this),6000);
 
 	},
 	trigger:function(event){
 		if( !this.listener[event] )
 			return
+		var args=[];
+		for(var i=1;i<arguments.length;i++)
+			args.push(arguments[i]);
 		for(var i=0;i<this.listener[event].length;i++)
-			this.listener[event][i]();
+			this.listener[event][i].apply(this,args);
 	},
 	on:function(event,fn){
 		if( !this.listener[event] )
@@ -125,9 +210,6 @@ GramListener.prototype={
 };
 
 (function($){
-
-
-	
 
 	var googleApiKey="AIzaSyBMU_Uuqf8WevUvNoRw3hONVjr8K_q5aIw";
 
@@ -156,13 +238,15 @@ GramListener.prototype={
 
 		$eventLayer:null,
 
+		$shadow:null,
+
 		isfocus:null,
 
 		latlng:null,
 
 		keylistener:null,
 
-		init:function( gramElement , map ){
+		init:function( gramElement , map , onChange ){
 
 			this.gramElement=gramElement;
 
@@ -184,20 +268,34 @@ GramListener.prototype={
 			.on( 'click' , $.proxy(function(e){
 				this.isfocus?this.unfocus():this.focus();
 				e.stopPropagation();
-			},this));
+			},this))
+			.on( 'mouseenter' , $.proxy(function(e){
+				this.$el.add( this.$shadow ).addClass('hover');
+			},this))
+			.on( 'mouseleave' , $.proxy(function(e){
+				this.$el.add( this.$shadow ).removeClass('hover');
+			},this))
+			
 
+			this.$shadow=$('<div class="gram-marker-shadow" style="position:absolute;">');
+
+			this.animDrop();
 		},
 
 		onAdd:function(){
 
 
 			var panes = this.getPanes();
-  			$( panes.overlayLayer ).append( this.$el );
+  			$( panes.overlayImage ).append( this.$el );
 
   			this.$eventLayer
   			.width( this.$el.width() )
   			.height( this.$el.height() )
   			.appendTo( $( panes.overlayMouseTarget ) );
+
+  			this.$shadow
+  			.appendTo( $( panes.overlayShadow ) );
+
 		},
 
 		focus:function(){
@@ -214,6 +312,7 @@ GramListener.prototype={
 			},this));
 
 		},
+
 		unfocus:function(){
 			
 			this.isfocus=false;
@@ -226,6 +325,14 @@ GramListener.prototype={
 
 		},
 
+		animDrop:function(){
+			if(this.$el.hasClass('drop-anim'))
+				return;
+			this.$el.add( this.$shadow ).addClass('drop-anim');
+			window.setTimeout($.proxy(function(){
+				this.$el.add( this.$shadow ).removeClass('drop-anim');
+			},this),600);
+		},
 
 		render:function(){
 
@@ -245,6 +352,9 @@ GramListener.prototype={
 				h=this.$el.height();
 
   			var p = overlayProjection.fromLatLngToDivPixel(this.latlng);
+
+  			var z=Math.floor( p.y );
+
  			p.x-=w/2;
  			p.y+=-h+4;
 
@@ -252,7 +362,7 @@ GramListener.prototype={
   			this.$el.css({ 
 				'top':  p.y+"px" ,
 				'left': p.x+"px",
-				'z-index':Math.floor(p.y) + (this.isfocus?1200:0)
+				'z-index':z
 			})
 
 			this.$el.find('.gram-marker-tick').css({ 
@@ -264,7 +374,12 @@ GramListener.prototype={
 				'height': h+"px",
 				'top':    p.y+"px" ,
 				'left':   p.x+"px",
-				'z-index':Math.floor(p.y) + (this.isfocus?1200:0)
+				'z-index':z
+			})
+
+			this.$shadow.css({ 
+				'top':  (p.y+10)+"px" ,
+				'left': (p.x+w/2-5)+"px",
 			})
 			
 		},
@@ -272,6 +387,7 @@ GramListener.prototype={
 		onRemove : function() {
 		  this.$el.detach();
 		  this.$eventLayer.detach();
+		  this.$shadow.detach();
 		},
 	});
 	};
@@ -281,7 +397,7 @@ GramListener.prototype={
 		this.$el=$el;
 		this.state="load";
 		this.model=options.model;
-
+		this.onChange=options.onChange;
 
 		var initMap=function(){
 	        this.map = new google.maps.Map( $el.get(0) , {
@@ -294,7 +410,8 @@ GramListener.prototype={
         this.loadApi( $.proxy(initMap,this) );
 
         if( this.model )
-        	this.model.on("change",$.proxy(this.render,this));
+        	//this.model.on("change",$.proxy(this.render,this));
+        	this.model.on("add",$.proxy(this.addOne,this));
 	};
 	GramMap.prototype={
 
@@ -303,6 +420,8 @@ GramListener.prototype={
 		map:null,
 
 		state:null,
+
+		onchange:null,
 
 		loadApi:function( onloaded ){
 			//is it already loaded?
@@ -345,7 +464,6 @@ GramListener.prototype={
 			}
 
 			$scr.appendTo($('head'));
-
 		},
 
 		render:function(){
@@ -373,9 +491,17 @@ GramListener.prototype={
 			return this;
 		},
 
+		addOne:function(gramElement){
+			var marker = new GramMarker(
+			    gramElement,
+			    this.map
+			);
+		},
+
 	};
 
 	$.fn.gramMap=function(options){
+		options=options||{};
 		this.each(function(){
 			var $el=$(this);
 			var grammap= new GramMap( $el , options );
@@ -385,3 +511,241 @@ GramListener.prototype={
 	};
 
 })($);
+
+
+
+
+(function($){
+
+	function GramElementView(){
+		this.init.apply(this,arguments);
+	};
+	GramElementView.prototype={
+
+		$el:null,
+
+		gramElement:null,
+
+		onChange:null,
+
+		init:function(gramElement,options){
+			options=options||{};
+			this.gramElement=gramElement;
+			this.onChange=options.onChange;
+
+			this.$el=$( '<div class="gram-list-element"><img></img></div>' );
+
+			this.render();
+
+		},
+
+		
+		render:function(){
+
+			this.$el.find('img').attr('src', this.gramElement.imageUrl );
+
+			return this;
+		},
+
+
+	}
+
+	function GramList($el,options){
+		this.$el=$el
+		.elegantContainer({
+			margin:15,
+		});
+
+		this.gramListener=options.gramListener;
+		this.onChange=options.onChange;
+		this.maxElement=options.maxElement;
+
+        if( this.gramListener )
+        	//this.gramListener.on("change",$.proxy(this.render,this));
+        	this.gramListener.on("add",$.proxy(this.addone,this));
+
+        this.gramElementViews=[];
+	};
+	GramList.prototype={
+
+		$el:null,
+
+		onchange:null,
+
+		gramListener:null,
+
+		gramElementViews:null,
+
+		addone:function(gramElement){
+
+			var el=new GramElementView(gramElement);
+
+			this.gramElementViews.push( el );
+
+			this.$el.elegantContainer('append', el.$el );
+		},
+
+		render:function(){
+
+			
+			return this;
+		},
+
+	};
+
+	$.fn.gramList=function(options){
+		options=options||{};
+		this.each(function(){
+			var $el=$(this);
+			var gramlist= new GramList( $el , options );
+			$el.data('gramList',gramlist);
+		});
+		return this;
+	};
+
+})($);
+
+
+
+
+
+
+
+
+
+
+(function($){
+
+
+	function ElegantContained($el,options){
+		this.$el=$el
+		.addClass('elegant-element');
+	};
+	ElegantContained.prototype={
+
+		$el:null,
+
+		moveTo:function(x,y,notransition){
+			if(notransition)
+				this.$el.removeClass('transition-enable');
+			this.$el.css({
+				'left':x+'px',
+				'top':y+'px',
+			});
+			if(notransition)
+				this.$el.addClass('transition-enable');
+		},
+
+	};
+
+	function ElegantContainer($el,options){
+		this.$el=$el;
+		this.q=[];
+		this.margin=options.margin==null?5:options.margin;
+
+		this.$c=$('<div style="position:absolute;">')
+		.appendTo( this.$el );
+
+		var qResize=window.onresize;
+		window.onresize=$.proxy(function(){
+			this.resize();
+			qResize();
+		},this);
+	};
+	ElegantContainer.prototype={
+
+		$el:null,
+
+		$c:null,
+
+		q:null,
+
+		margin:null,
+
+		addToQueu:function( $el ){
+			this.q.unshift( new ElegantContained($el) );
+			this.$c.append($el);
+			this.resize();
+		},
+
+		resize:function(){
+
+			if(this.q.length==0)
+				return this;
+
+			var cw=this.$el.width(),
+				ch=this.$el.height();
+
+			var ew=this.q[0].$el.width(),
+				eh=this.q[0].$el.height();
+
+			var margin=this.margin;
+
+			var ncol=Math.floor( ( cw - margin )/( ew + margin ) );
+
+			var trueMargin=(cw-ncol*ew)/(ncol+1);
+
+			var xc=-1,yc=0;
+
+			var i=0;
+			while(i<this.q.length){
+				if(yc%2==0){
+					xc++;
+					if(xc>=ncol){
+						yc++;
+						xc=ncol-1;
+					}
+				}else{
+					xc--;
+					if(xc<0){
+						yc++;
+						xc=0;
+					}
+				}
+
+				this.q[i].moveTo(
+					xc * ( trueMargin + ew ) + trueMargin,
+					yc * ( margin + eh ) + margin
+				);
+				i++;
+			}
+
+			this.$el.height( (yc+1) * ( margin + eh ) + margin )
+
+		},
+	};
+
+	$.fn.elegantContainer=function(options){
+		options=options||{};
+		var args=arguments;
+		this.each(function(){
+			var $el=$(this);
+			if( !$el.data('elegantContainer') ){
+				var elegantContainer= new ElegantContainer( $el , options );
+				$el.data('elegantContainer',elegantContainer);
+			}
+			if( typeof(options)=="string" ){
+				if(options=="append")
+					$el.data('elegantContainer').addToQueu(args[1]);
+			}
+		});
+		return this;
+	};
+
+})($);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
