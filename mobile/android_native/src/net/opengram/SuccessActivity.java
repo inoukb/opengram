5775834786223350becc93cf3e9a93a6f7f1f647
Vagaweb <fabien.casters@gmail.com>
2013-05-21 23:39:41 +0200

    Android : Add button for Google map

--- mobile/android_native/src/net/opengram/SuccessActivity.java
+++ mobile/android_native/src/net/opengram/SuccessActivity.java
@@ -2,13 +2,33 @@ package net.opengram;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
 
-public class SuccessActivity extends Activity
+public class SuccessActivity extends Activity implements OnClickListener
 {
+    private Button _mapButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.success);
+
+        _mapButton = (Button) findViewById(R.id.map);
+        _mapButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View button)
+    {
+        if (button == _mapButton)
+        {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vagaweb.fr"));
+            startActivity(intent);
+        }
     }
 }
